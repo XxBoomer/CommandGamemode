99d19898a5d03226be949f77397f5d620bf0850f
CentOsDogE <XxBoomer@users.noreply.github.com>
2017-12-16 21:19:40 +0700

    Create Main.php

---
+++ src/CommandGamemode/Main.php
@@ -0,0 +1,51 @@
+<?php
+namespace BasicCommands;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Command\Command;
+use pocketmine\Command\CommandSender;
+use pocketmine\entity\Effect;
+class Main extends PluginBase{
+	
+	public function onEnable(){
+		$this->getServer()->getLogger()->info("BasicCommands enabled!");
+	}
+	
+	public function onDisable(){
+		$this->getServer()->getLogger()->info("BasicCommands disabled!");
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+		
+		switch($cmd->getName()){
+			 
+      case "gms":
+      if($sender->hasPermission("gms.command")} {         
+         $sender->setGamemode(0);
+         $sender->sendMessage("§aBạn đã đc chuyễn thành sinh tồn");
+         if(!$sender->hasPermission("gms.command")} {
+            $sender->sendMessage("§cBạn không có quyền để xài command");
+         }
+      break;
+      			 
+      case "gmc":
+      if($sender->hasPermission("gmc.command")} {         
+         $sender->setGamemode(1);
+         $sender->sendMessage("§aBạn đã đc chuyễn thành sáng tạo");
+         if(!$sender->hasPermission("gmc.command")} {
+            $sender->sendMessage("§cBạn không có quyền để xài command");
+         }
+      break;
+      			 
+      case "gmt":
+      if($sender->hasPermission("gmt.command")} {         
+         $sender->setGamemode(0);
+         $sender->sendMessage("§aBạn đã đc chuyễn thành theo dõi");
+         if(!$sender->hasPermission("gmt.command")} {
+            $sender->sendMessage("§cBạn không có quyền để xài command");
+         }
+      break;
+    }
+    return true;
+  }
+}
